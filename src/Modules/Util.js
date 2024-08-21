0f872284c4f412c0c1cbeef47cd5cc4a4d4881a6
SinisterDev <thesinisterdev@gmail.com>
2024-08-21 15:25:43 +0530

    Update Route Handler

--- src/Modules/Util.js
+++ src/Modules/Util.js
@@ -1,17 +1,36 @@
 const chalk = require('chalk');
 const log = console.log;
+const { RateLimiterMemory } = require('rate-limiter-flexible');
 
-// Going to fix this once we decide the security protocol (this was copied and pasted from the quiz project)
-// function authenticateEndpoint(App, req, ignoreEndpoints) {
-// 	if (!ignoreEndpoints.includes(req.path))
-// 		return !(
-// 			!req.get('Authorization') ||
-// 			!App.players.has(req.get('Authorization').split(' ')[1])
-// 		);
-// 	else return true;
-// }
+const rateLimiter = new RateLimiterMemory({
+	points: process.env.MAX_REQUESTS_PER_SECOND || 4,
+	duration: 1,
+});
+
+const rateLimiterMiddleware = (req, res, next) => {
+	const token = req.get('Authorization')?.split(' ')[1] || req.ip;
+	rateLimiter
+		.consume(token, 1)
+		.then(() => {
+			next();
+		})
+		.catch(() => {
+			res.status(429).send('Too Many Requests');
+		});
+};
+
+function authenticateAdmin(req, ignoreRoutes) {
+	if (!ignoreRoutes.includes(req.path))
+		return !(
+			!req.get('Authorization') ||
+			!(process.env.ADMIN === req.get('Authorization').split(' ')[1])
+		);
+	else return true;
+}
 
 module.exports = {
+	rateLimiterMiddleware,
+	authenticateAdmin,
 	err(text) {
 		log(chalk.italic.bold.redBright(text + '\n'));
 	},
--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ const compression = require('compression');
 const App = express();
 require('dotenv').config({ path: '../.env' });
 const mongoose = require('mongoose');
-const { err, success, misc, blue, caution } = require('./Modules/Util');
+const { err, success, misc, blue, authenticateAdmin, rateLimiterMiddleware, caution } = require('./Modules/Util');
 
 const uid = new ShortUniqueId({ length: 10 });
+
 const adminRoutes = fs
 	.readdirSync('./Routes/admin')
 	.filter((file) => file.endsWith('.js'));
@@ -18,8 +19,9 @@ const userRoutes = fs
 
 App.use(express.json());
 App.use(compression());
+App.disable('x-powered-by');
+App.use(rateLimiterMiddleware);
 App.admin = uid.rnd();
-
 
 // -----------------CONNECTION-----------------------------------------------------
 
@@ -34,7 +36,7 @@ async function connect() {
 		.then(() => success('Established connection with Database'))
 		.catch((error) => err(error));
 
-	handleRoutes();
+	await handleRoutes();
 
 	App.listen(process.env.PORT, () => {
 		misc(
@@ -45,29 +47,97 @@ async function connect() {
 
 // ------------- ROUTES-HANDLER---------------------------------------------------------------
 
-function handleRoutes() {
-	const methods = ['get', 'post', 'put', 'delete'];
-	const dirs = [adminRoutes, userRoutes];
-	dirs.forEach((dir) => {
-		for (const file of dir) {
-			const endpoint = require(
-				`./Routes/${dir === adminRoutes ? 'admin' : 'user'}/${file}`,
-			);
+const routePaths = {
+	adminRoutes: 'admin',
+	userRoutes: 'user',
+};
 
-			(async function (App) {
-				const path = `/${dir === adminRoutes ? 'admin' : 'user'}/${endpoint.name}`;
-				methods.forEach((method) =>
-					App[method](path, async (req, res) => {
-						await endpoint[method](App, req, res);
-					}),
-				);
-			})(App);
+const routeRegistry = new Map();
+
+async function loadEndpoint(routePath, fileName) {
+	const endpointPath = path.join(__dirname, 'Routes', routePath, fileName);
+	return require(endpointPath);
+}
+
+async function registerRoute(App, basePath, methods) {
+	const [fileName] = basePath.split('/').slice(-1);
+	const endpoint = await loadEndpoint(
+		basePath.split('/').slice(1, -1).join('/'),
+		`${fileName}.js`,
+	);
+
+	methods.forEach((method) => {
+		if (endpoint[method]) {
+			App[method](basePath, async (req, res) => {
+				try {
+					if (
+						basePath.startsWith('/admin') &&
+						!authenticateAdmin(req, ['/admin/login'])
+					) {
+						return res.sendStatus(401);
+					}
+					await endpoint[method](App, req, res);
+				} catch (error) {
+					err(`Error handling route: ${error}`);
+					res.sendStatus(500);
+				}
+			});
+			routeRegistry.set(
+				`${basePath}_${method.toLowerCase()}`,
+				endpoint[method],
+			);
 		}
 	});
 }
 
+async function handleRequest(req, res) {
+	const { method, originalUrl } = req;
+	const handler = routeRegistry.get(`${originalUrl}_${method.toLowerCase()}`);
+
+	if (handler) {
+		try {
+			if (
+				originalUrl.startsWith('/admin') &&
+				!authenticateAdmin(req, ['/admin/login'])
+			) {
+				return res.sendStatus(401);
+			}
+			await handler(req, res);
+		} catch (error) {
+			err(`Error handling route: ${error}`);
+			res.sendStatus(500);
+		}
+	} else {
+		caution(
+			`Request sent from IP: ${req.ip} for invalid method  ${method}: ${originalUrl}`,
+		);
+		res.sendStatus(404);
+	}
+}
+
+async function handleRoutes() {
+	const methods = ['get', 'post', 'put', 'delete'];
+	const routeDirs = [adminRoutes, userRoutes];
+
+	for (const dir of routeDirs) {
+		const routePath =
+			routePaths[dir === adminRoutes ? 'adminRoutes' : 'userRoutes'];
+		const fileNames = await fs.promises.readdir(
+			path.join(__dirname, 'Routes', routePath),
+		);
+
+		for (const fileName of fileNames) {
+			if (fileName.endsWith('.js')) {
+				const basePath = `/${routePath}/${path.basename(fileName, '.js')}`;
+				await registerRoute(App, basePath, methods);
+			}
+		}
+	}
+	App.use(handleRequest);
+	success('Registered all Routes');
+}
 // ---------------------------------------------------------------------------------------------
 
-connect().catch((e) => err(e));
+connect().catch((e) => console.log(e));
 
 
